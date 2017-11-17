c2994af0dba4e50fa00ecf55c7675de3ac4312ae
Juergen Konrad <juergen.konrad@ericsson.com>
2017-11-17 12:36:40 +0100

    add server changes

--- speaker-recognition/src/main/java/com/ericsson/ai/speaker/app/SpeakerIdentificationRestControlller.java
+++ speaker-recognition/src/main/java/com/ericsson/ai/speaker/app/SpeakerIdentificationRestControlller.java
@@ -1,9 +1,14 @@
 package com.ericsson.ai.speaker.app;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +34,14 @@ public class SpeakerIdentificationRestControlller
         _identificationService.createSpeakerProfile(pRequestProfile);
     }
 
+    @GetMapping("/speaker/voice")
+    public InputStream getVoice()
+    {
+    	InputStream s = getDefaultIdentificationAudioStream();
+        return s;
+    }
+
+
     @GetMapping("/speaker/{profileId}")
     public SpeakerProfile getSpeakerName(@PathVariable(name="profileId") String pProfileId)
     {
@@ -37,15 +50,62 @@ public class SpeakerIdentificationRestControlller
 
     @PutMapping("/enroll/speaker/{profileId}")
     public EnrollmentOperation enrollSpeaker(@PathVariable(name="profileId") String pSpeakerProfileId,
-                                   @RequestBody InputStream pAudioStream)
+                                   @RequestBody byte[] pAudioFile)
     {
-        return _identificationService.enrollSpeaker(pAudioStream, UUID.fromString(pSpeakerProfileId));
+    	return _identificationService.enrollSpeaker(getDefaultEnrollmentAudioStream(),
+    			UUID.fromString(pSpeakerProfileId));
+//    	return _identificationService.enrollSpeaker(
+//    			pAudioFile == null ? getDefaultEnrollmentAudioStream() : getAudioStreamFromFile(pAudioFile.toString()),
+//    			UUID.fromString(pSpeakerProfileId));
+//        return _identificationService.enrollSpeaker(pAudioStream, UUID.fromString(pSpeakerProfileId));
     }
 
     @PutMapping("/identify")
-    public IdentificationOperation verify(@RequestBody InputStream pAudioStream)
+    public IdentificationOperation verify(@RequestBody byte[] pAudioFile)
     {
-        return _identificationService.identifySpeaker(pAudioStream);
+    	return _identificationService.identifySpeaker(getDefaultIdentificationAudioStream());
+
+//        return _identificationService.identifySpeaker(pAudioFile == null ? getDefaultIdentificationAudioStream() : getAudioStreamFromFile(pAudioFile.toString()));
+//        return _identificationService.identifySpeaker(pAudioStream);
     }
+
+
+	private InputStream getAudioStreamFromFile(String pAudioFile)
+	{
+		try
+		{
+			return new FileInputStream(pAudioFile);
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private InputStream getDefaultIdentificationAudioStream()
+	{
+		try
+		{
+			return new FileInputStream("./src/test/resources/IdentifyJuergen1.wav");
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private InputStream getDefaultEnrollmentAudioStream()
+	{
+		try
+		{
+			return new FileInputStream("./src/test/resources/EnrollJuergen1.wav");
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
- 
--- speaker-recognition/src/test/java/com/ericsson/ai/speaker/app/SpeakerApplicationTest.java
+++ speaker-recognition/src/test/java/com/ericsson/ai/speaker/app/SpeakerApplicationTest.java
@@ -35,7 +35,9 @@ import com.microsoft.cognitive.speakerrecognition.contract.verification.Verifica
 public class SpeakerApplicationTest
 {
 	private static final String SUBSCRIPTION_KEY = "b32caa3d5f2046558b255a5c04bf198f";
-	private static final String SPEAKER_PROFILE_UUID = "5ef9d03c-04ea-46ba-afff-ed98187ff775";
+	private static final String SPEAKER_IDENTIFICATION_PROFILE_UUID = "5ef9d03c-04ea-46ba-afff-ed98187ff775";
+//	private static final String SPEAKER_VERIFICATION_PROFILE_UUID = SPEAKER_IDENTIFICATION_PROFILE_UUID; // verification requires its own profileID
+	private static final String SPEAKER_VERIFICATION_PROFILE_UUID = "";
 	private static final String LOCALE_EN_US = "en-us";
 	private static final long WAIT_BEFORE_STATUS_CHECK = 3000;
     private static final boolean FORCE_SHORT_AUDIO = true;
@@ -50,8 +52,10 @@ public class SpeakerApplicationTest
 	InputStream _identificationEnrollmentStream2 = null;
 	InputStream _identificationInputStream = null;
 
-	InputStream _verificationEnrollmentStream = null;
-	InputStream _verificationInputStream = null;
+	InputStream _verificationPhrase1 = null;
+	InputStream _verificationPhrase2 = null;
+	InputStream _verificationPhrase3 = null;
+//	InputStream _verificationInputStream = null;
 
 
 	@Before
@@ -80,8 +84,9 @@ public class SpeakerApplicationTest
 			_identificationInputStream = new FileInputStream("./src/test/resources/IdentifyJuergen1.wav");
 
 			// TODO: create wav files for verification
-			_verificationEnrollmentStream = new FileInputStream("tobecreated.wav");
-			_verificationInputStream =  new FileInputStream("tobecreated.wav");
+			_verificationPhrase1 = new FileInputStream("./src/test/resources/VerificationPhrase1.wav");
+			_verificationPhrase2 = new FileInputStream("./src/test/resources/VerificationPhrase2.wav");
+			_verificationPhrase3 = new FileInputStream("./src/test/resources/VerificationPhrase3.wav");
 		}
 		catch(FileNotFoundException e)
 		{
@@ -108,7 +113,7 @@ public class SpeakerApplicationTest
 				"profile = " + p.identificationProfileId.toString() + "   status = " + p.enrollmentStatus
 				));
 
-		assertTrue(speakerProfiles.stream().anyMatch(p->p.identificationProfileId.toString().equals(SPEAKER_PROFILE_UUID)));
+		assertTrue(speakerProfiles.stream().anyMatch(p->p.identificationProfileId.toString().equals(SPEAKER_IDENTIFICATION_PROFILE_UUID)));
 	}
 
 	/**
@@ -160,7 +165,7 @@ public class SpeakerApplicationTest
     @Test
     public void testSpeakerIdentificationClientEnrollLongAudio() throws EnrollmentException, IOException, InterruptedException
     {
-        OperationLocation operationLocation = _speakerIdentificationClient.enroll(_identificationEnrollmentStream1, UUID.fromString(SPEAKER_PROFILE_UUID));
+        OperationLocation operationLocation = _speakerIdentificationClient.enroll(_identificationEnrollmentStream1, UUID.fromString(SPEAKER_IDENTIFICATION_PROFILE_UUID));
         if(operationLocation != null)
         {
         	System.out.println(operationLocation.Url);
@@ -187,7 +192,7 @@ public class SpeakerApplicationTest
     @Test
     public void testSpeakerIdentificationClientEnrollShortAudio() throws EnrollmentException, IOException, InterruptedException
     {
-        OperationLocation operationLocation = _speakerIdentificationClient.enroll(_identificationEnrollmentStream2, UUID.fromString(SPEAKER_PROFILE_UUID), FORCE_SHORT_AUDIO);
+        OperationLocation operationLocation = _speakerIdentificationClient.enroll(_identificationEnrollmentStream2, UUID.fromString(SPEAKER_IDENTIFICATION_PROFILE_UUID), FORCE_SHORT_AUDIO);
         if(operationLocation != null)
         {
         	System.out.println(operationLocation.Url);
@@ -214,7 +219,7 @@ public class SpeakerApplicationTest
     @Test
     public void testSpeakerIdentificationClientIdentify() throws IdentificationException, IOException, InterruptedException
     {
-    	List<UUID> allProfileIds = Arrays.asList(UUID.fromString(SPEAKER_PROFILE_UUID));
+    	List<UUID> allProfileIds = Arrays.asList(UUID.fromString(SPEAKER_IDENTIFICATION_PROFILE_UUID));
     	OperationLocation operationLocation = _speakerIdentificationClient.identify(_identificationInputStream, allProfileIds, FORCE_SHORT_AUDIO);
 
         if(operationLocation != null)
@@ -228,7 +233,7 @@ public class SpeakerApplicationTest
         	System.out.println(identificationOperation.processingResult.confidence);
 
         	// assert that identification was successful
-        	assertEquals(SPEAKER_PROFILE_UUID, identificationOperation.processingResult.identifiedProfileId.toString());
+        	assertEquals(SPEAKER_IDENTIFICATION_PROFILE_UUID, identificationOperation.processingResult.identifiedProfileId.toString());
         }
     }
 
@@ -242,7 +247,7 @@ public class SpeakerApplicationTest
     @Test
     public void testSpeakerIdentificationServiceEnroll()
     {
-    	EnrollmentOperation enrollmentOperation = _speakerIdentificationService.enrollSpeaker(_identificationEnrollmentStream1, UUID.fromString(SPEAKER_PROFILE_UUID));
+    	EnrollmentOperation enrollmentOperation = _speakerIdentificationService.enrollSpeaker(_identificationEnrollmentStream1, UUID.fromString(SPEAKER_IDENTIFICATION_PROFILE_UUID));
 
     	System.out.println(enrollmentOperation.processingResult.enrollmentSpeechTime);
     	System.out.println(enrollmentOperation.processingResult.remainingEnrollmentSpeechTime);
@@ -262,18 +267,53 @@ public class SpeakerApplicationTest
     	System.out.println(identificationOperation.processingResult.confidence);
 
     	// assert that identification was successful
-    	assertEquals(SPEAKER_PROFILE_UUID, identificationOperation.processingResult.identifiedProfileId.toString());
+    	assertEquals(SPEAKER_IDENTIFICATION_PROFILE_UUID, identificationOperation.processingResult.identifiedProfileId.toString());
     }
 
+
+    //
+    // Test verification client
+    //
+	/**
+	 * Test creating a speaker profile
+	 * Deactivate this test, once the speaker profileID has been obtained
+	 *
+	 * @throws CreateProfileException
+	 * @throws IOException
+	 */
+	//@Test
+    public void testSpeakerVerificationClientCreateProfile() throws CreateProfileException, IOException
+    {
+    	com.microsoft.cognitive.speakerrecognition.contract.verification.CreateProfileResponse profileResponse = _speakerVerificationClient.createProfile(LOCALE_EN_US);
+		String speakerProfileUUID = profileResponse.verificationProfileId.toString();
+		System.out.println(speakerProfileUUID);
+    }
 
     //
     // Test verification service
     //
 
+    /**
+     *
+     */
     @Test
     public void testSpeakerVerificationServiceEnroll()
     {
-    	Enrollment enrollment = _speakerVerificationService.enrollPhrase(_verificationEnrollmentStream, UUID.fromString(SPEAKER_PROFILE_UUID));
+    	Enrollment enrollment = _speakerVerificationService.enrollPhrase(_verificationPhrase1, UUID.fromString(SPEAKER_VERIFICATION_PROFILE_UUID));
+
+    	System.out.println(enrollment.enrollmentsCount);
+    	System.out.println(enrollment.remainingEnrollments);
+    	System.out.println(enrollment.phrase);
+    	System.out.println(enrollment.enrollmentStatus);
+
+    	enrollment = _speakerVerificationService.enrollPhrase(_verificationPhrase2, UUID.fromString(SPEAKER_VERIFICATION_PROFILE_UUID));
+
+    	System.out.println(enrollment.enrollmentsCount);
+    	System.out.println(enrollment.remainingEnrollments);
+    	System.out.println(enrollment.phrase);
+    	System.out.println(enrollment.enrollmentStatus);
+
+    	enrollment = _speakerVerificationService.enrollPhrase(_verificationPhrase3, UUID.fromString(SPEAKER_VERIFICATION_PROFILE_UUID));
 
     	System.out.println(enrollment.enrollmentsCount);
     	System.out.println(enrollment.remainingEnrollments);
@@ -284,7 +324,7 @@ public class SpeakerApplicationTest
     @Test
     public void testSpeakerVerificationServiceIdentify()
     {
-    	Verification verification = _speakerVerificationService.verifySpeaker(_verificationInputStream);
+    	Verification verification = _speakerVerificationService.verifySpeaker(_verificationPhrase2);
 
     	System.out.println(verification.result);
     	System.out.println(verification.phrase);
