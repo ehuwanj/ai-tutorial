d488cd913576c8d8488b6d99ee00d4287ed20b36
huangw <wanjun.huang@ericsson.com>
2017-11-17 16:50:18 +0100

    add REST put function

--- speaker-ui/src/app/services/rest/rest.service.ts
+++ speaker-ui/src/app/services/rest/rest.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { RequestOptions, ResponseContentType } from '@angular/http';
+import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class RestService {
 
-  constructor() { }
+  private BASE_URI = 'http://localhost:8080/';
 
+  constructor(private httpClient: HttpClient) { }
 
-  public post(URL){
+  public get(apiURL){
+    
+    return this.httpClient.get(this.BASE_URI + apiURL);
+  }
 
+  public put(apiURL: String, data:any){
+
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    return this.httpClient.put(this.BASE_URI + apiURL, data, httpOptions);
   }
 
 }
